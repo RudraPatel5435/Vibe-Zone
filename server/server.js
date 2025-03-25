85113c1b28fbd2ce2e6592b0561294d712371cd5
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-03-25 08:20:55 +0530

    Deployment

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+require('dotenv').config()
 const app = express();
 const http = require("http");
+const PORT = process.env.port || 5500
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
@@ -61,6 +63,6 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("listening on *:3000");
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
